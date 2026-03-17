467a26cbada78bda9cae7a3c92bf912b06da3dfc
Kyle Kingsbury <aphyr@aphyr.com>
2026-03-16 22:25:02 -0500

    Version 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen.mysql "0.0.1"
+(defproject jepsen.mysql "0.0.2"
   :description "Tests for MySQL with read replicas"
   :url "https://github.com/jepsen-io/jepsen"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
